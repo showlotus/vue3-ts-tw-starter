12475a73f776e07d783c39604e76a92ac46c880f
showlotus <showlotus2000@gmail.com>
2026-04-29 16:23:33 +0800

    *

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -52,44 +52,5 @@ const store = useStore()
       See <code>vite.config.ts</code> and <code>src/env.d.ts</code> for the configuration and
       <code>src/App.vue</code> for usage example.
     </p>
-
-    <h3>Code Coverage</h3>
-
-    <p>
-      Code coverage is provided from unit + component tests by
-      <a href="https://vitest.dev/guide/coverage.html">Vitest and V8</a>.
-    </p>
-
-    <p>
-      E2E coverage is a bit trickier to do because of the way Vite works. Typical Vite pipelines don't use babel at all
-      which is needed above for automatically instrument the transpilated code. Vite is powered by eslint which has
-      decided <a href="https://github.com/evanw/esbuild/issues/184"> code coverage being out of scope </a>.
-    </p>
-
-    <h2>Elsewhere</h2>
-
-    <ul>
-      <li>Follow <a href="https://twitter.com/uninen">@Uninen on Twitter</a></li>
-      <li>
-        Read continuously updating learnings from Vite / Vue / TypeScript and other Web development topics on my
-        <a href="https://til.unessa.net/">Today I Learned</a> site
-      </li>
-    </ul>
-
-    <h2>Contributing</h2>
-
-    <p>
-      Contributions are welcome! Please follow the
-      <a href="https://www.contributor-covenant.org/version/2/0/code_of_conduct/">code of conduct</a>
-      when interacting with others.
-    </p>
   </main>
-  <footer>
-    <p>
-      vite-ts-tailwind-starter v{{ store.appMeta.version }} by <a href="https://twitter.com/uninen">@Uninen</a> &copy;
-      2020-{{ new Date().getFullYear() }}.
-      <template v-if="store.appMeta.builtAt"> Site built {{ store.appMeta.builtAt.toLocaleDateString() }}. </template>
-      <template v-else> Development mode. </template>
-    </p>
-  </footer>
 </template>
